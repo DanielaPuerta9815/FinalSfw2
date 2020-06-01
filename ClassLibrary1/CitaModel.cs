c730f5c585c95a9959d39b39ba3b01c38c9706cd
Bryan Smith <brperez@ulasallista.edu.co>
2020-06-01 13:16:00 -0500

    App Consola

---
+++ ClassLibrary1/CitaModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public DataTable szs()
+        {
+
+            DataTable dataTable = new DataTable();
+        //int idProdcuto = 15429219;
+
+        var url = $"http://localhost:51971/api/Medico?nombreCentro=ClinicaAntioquia";
+        var request = (HttpWebRequest)WebRequest.Create(url);
+        request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return dataTable;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            string responseBody = objReader.ReadToEnd().Replace("\\","");
+
+                            return JsonStringToDataTable(responseBody);
+                        }
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+        public DataTable JsonStringToDataTable(string jsonString)
+        {
+            DataTable dt = new DataTable();
+            string[] jsonStringArray = Regex.Split(jsonString.Replace("[", "").Replace("]", ""), "},{");
+            List<string> ColumnsName = new List<string>();
+            foreach (string jSA in jsonStringArray)
+            {
+                string[] jsonStringData = Regex.Split(jSA.Replace("{", "").Replace("}", ""), ",");
+                foreach (string ColumnsNameData in jsonStringData)
+                {
+                    try
+                    {
+                        int idx = ColumnsNameData.IndexOf(":");
+                        string ColumnsNameString = ColumnsNameData.Substring(0, idx - 1).Replace("\"", "");
+                        if (!ColumnsName.Contains(ColumnsNameString))
+                        {
+                            ColumnsName.Add(ColumnsNameString);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(string.Format("Error Parsing Column Name : {0}", ColumnsNameData));
+                    }
+                }
+                break;
+            }
+            foreach (string AddColumnName in ColumnsName)
+            {
+                dt.Columns.Add(AddColumnName);
+            }
+            foreach (string jSA in jsonStringArray)
+            {
+                string[] RowData = Regex.Split(jSA.Replace("{", "").Replace("}", ""), ",");
+                DataRow nr = dt.NewRow();
+                foreach (string rowData in RowData)
+                {
+                    try
+                    {
+                        int idx = rowData.IndexOf(":");
+                        string RowColumns = rowData.Substring(0, idx - 1).Replace("\"", "");
+                        string RowDataString = rowData.Substring(idx + 1).Replace("\"", "");
+                        nr[RowColumns] = RowDataString;
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+                dt.Rows.Add(nr);
+            }
+            return dt;
+        }
+    }
+}
